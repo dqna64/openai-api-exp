43054f8362d3564286ba8c05eb336a9c87b94664
Gordon Huang <dqna64@gmail.com>
2023-07-08 03:08:44 +1000

    set up openai api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+
+  const response = await fetch('http://localhost:3000/api/chat')
+  const data = await response.json()
+  console.log(data)
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
---
+++ src/pages/api/chat/index.ts
@@ -0,0 +1,68 @@
+import openai from '@/utils/openai'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse<{chatCompletionContent: string, error?: string}>) {
+
+    try {
+        const chatCompletion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content: "You are a helpful assistant whose job is to help the user study their areas of interest and learn the most important knowledge in those areas."
+                },
+                {
+                    role: "user",
+                    content: "Hello! Recently I've been reading about the Manhattan Project and it is fascinating."
+                }
+            ],
+            n: 2,
+            max_tokens: 200,
+            temperature: 1.2
+        })
+        
+        console.log(chatCompletion.data.choices[0].message)
+
+        console.log(res)
+        
+        res.status(200).json({chatCompletionContent: chatCompletion.data.choices[0].message?.content ?? "No content"})
+    }
+    catch(error: any) {
+        console.log(error)
+        res.status(500).json({chatCompletionContent: "Error", error: error.message})
+    }
+}
+
+const reqBody = {
+  "model": "gpt-3.5-turbo",
+  "messages": [{"role": "system", "content": "You are a helpful assistant whose job is to help the user study their areas of interest and learn the most important knowledge in those areas."}, {"role": "user", "content": "Hello! Recently I've been reading about the Manhattan Project and it is fascinating."}],
+  "n": 2,
+  "max_tokens": 200,
+  "temperature": 0
+}
+
+const curlReq = `curl https://api.openai.com/v1/chat/completions \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer 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" \
+-d '${JSON.stringify(reqBody)}'`
+
+// console.log(curlReq)
+
+/*
+curl https://api.openai.com/v1/chat/completions \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer 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" \
+-d '{"model":"gpt-3.5-turbo","messages":[{"role":"system","content":"You are a helpful assistant whose job is to help the user study their areas of interest and learn the most important knowledge in those areas."},{"role":"user","content":"Hello! Recently I've been reading about the Manhattan Project and it is fascinating."}],"n":2,"max_tokens":200,"temperature":0}'
+*/
+
+/*
+
+Potentially useful request body parameters to POST https://api.openai.com/v1/chat/completions
+- functions
+- temperature
+- top_p
+- max_tokens
+- stream
+- user
+
+*/
